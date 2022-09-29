65cdb1b3927dc6bddf5d1f6080bfb01b1e0afe62
israfaisal12345 <arifisra480@gmail.com>
2022-09-29 22:15:04 +0500

    working folder

---
+++ app.js
@@ -0,0 +1,26 @@
+
+// function readmore(){
+
+//   var text =document.getElementById('txt').innerHTML
+
+// var sir = document.getElementById('par')
+//   sir.innerHTML = text
+
+// }
+// function ras(){
+//   var is = document.getElementById('isra').innerHTML
+//   var tex = ('vcwvl vonewo ovenv elv rvnewob orgvn'
+  
+function squar(){
+  var s = document.getElementById('chakor')
+  s.className ='hidn'
+}
+function circle(){
+var c = document.getElementById('circl')
+c.className = 'saqr'
+}
+
+function tri(){
+  var t = document.getElementById('tri')
+  t.className ='triang'
+}
